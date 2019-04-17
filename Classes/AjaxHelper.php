42bcd72cbc9d2f6c93a67c15255b2ff6a79be71e
Stanislaw Gutsch <sg@kreativrudel.de>
2019-04-17 16:15:10 +0200

    ✨ added Ajax helper with json view rendering

---
+++ Classes/AjaxHelper.php
@@ -0,0 +1,33 @@
+<?php
+namespace Kreativrudel\Helpers;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+
+class AjaxHelper {
+    /**
+     * @var ObjectManager $objectManager
+     */
+    protected $objectManager;
+
+    public function __construct() {
+        $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
+    }
+
+    /**
+     * Appends variables and its configuration names to the json view
+     *
+     * @param mixed $jsonView
+     * @param array $variables
+     * @return string
+     */
+    public function renderJsonView($jsonView, array $variables = array()): string {
+        $variablesToRender = [];
+        foreach ($variables as $name => $variable) {
+            $jsonView->assign($name, $variable);
+            $variablesToRender[] = $name;
+        }
+        $jsonView->setVariablesToRender($variablesToRender);
+        return $jsonView->render();
+    }
+}
